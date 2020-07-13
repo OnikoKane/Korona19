96fab545160dc55502d5a310a82a0c1fd52bd2c3
KaneHuang <53499971+OnikoKane@users.noreply.github.com>
2020-07-13 11:34:59 +0800

    v1.3

--- se/urls.py
+++ se/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from se import views
 
-# app_name = 'se'
-
-# urlpatterns = [
-#     path('', views.index, name='index'),
-# ]
+app_name = 'se'
+urlpatterns = [
+    path('search/', views.korona),
+    # 可遵循rf将views改为api文件
+]
--- se/views.py
+++ se/views.py
@@ -1,28 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-# Create your views here.
-from django.views import generic
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from se.utils.ESSearch import ESService
 from se.utils.graphSearch import GraphService
 from se.utils.seq2tokens import WordHandler
+import json
 
 # py manage.py runserver
 
-# from se.__init__ import gs, ess, wordHandler
 
 gs = GraphService()
 ess = ESService()
 wordHandler = WordHandler()
 
 
-def korona19(request):
-    if request.method == 'GET':
-        return render(request, "korona19.html")
-
+@api_view(['GET', 'POST'])
+def korona(request):
     if request.method == "POST":
-        sequence = request.POST.get("sequence")
-        if sequence == '':
-            return render(request, "korona19.html")
+    #   sequence = request.GET.get("sequence")
+        data = json.loads(request.body)
+        sequence = data.get("sequence")
 
         # cut token for search(both)
         esword = wordHandler.esHandler(sequence=sequence)
@@ -30,33 +28,32 @@ def korona19(request):
 
         # Search
         esRt = ess.search(tokens=esword)
-
         # if flag is -1, then no content of graph for showing,
         if graphword != -1:
             graphRt = gs.search(value=graphword)
         else:
             graphRt = -1
 
+        rt = {
+            'graphRt': graphRt,
+            'esRt': list(esRt.values('value', 'label', 'graph')),
+        }
+        return Response(json.dumps(rt))
 
-        # ==============print==============
-        print('=========')
-        print(esword)
-        print(graphword)
-        print('=========')
-
-        print(graphRt)
-        print('==========')
-        for i in esRt:
-            print(i.label, ':', i.value)
-        # ==============print==============
-
-        return render(request, "search.html",
-                      {
-                          'graphRt': graphRt,
-                          'esRt': esRt,
-                      })
-    render(request, 'korona19.html')
-
-# def search(request):
-#
-#     return render(request, "search.html")
+# def korona(request):
+#     global graphRt
+#     if request.method == "POST":
+#         data = json.loads(request.body)
+#         sequence = data.get("sequence")
+#         esword = wordHandler.esHandler(sequence=sequence)
+#         graphword = wordHandler.graphHandler(sequence=sequence)
+#         esRt = ess.search(tokens=esword)
+#         esRt = esRt['hits']
+#         # neo4j数据库中存在则返回数据
+#         if graphword != -1:
+#             graphRt = gs.search(value=graphword)
+#         # 不存在则返回-1
+#         if graphword == -1:
+#             graphRt = -1
+#         result = {'graphRt': graphRt, 'esRt': esRt}
+#         return HttpResponse(json.dumps(result))
